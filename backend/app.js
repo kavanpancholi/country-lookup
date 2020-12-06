1f4942d1ddc086c2f3f5dd2cb81bfff89145c90c
Kavan Pancholi <kavanpancholi@gmail.com>
2020-12-07 00:09:56 +0530

    create 404 response for invalid requests

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ import cors from 'cors';
 import publicRoutes from './src/routes/public';
 import apiRoutes from './src/routes/api';
 import apiMiddleware from './src/middleware/apiAuth';
-import errorHandler from './src/middleware/errorHandler';
+import { errorResponse } from './src/helpers';
 
 dotenv.config();
 require('./src/config/sequelize');
@@ -37,6 +37,19 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/', publicRoutes);
 app.use('/api', [apiMiddleware, apiLimiter], apiRoutes);
-app.use(errorHandler);
-
+app.use((err, req, res) => {
+  if (err && err.message === 'validation error') {
+    const messages = err.errors.map(e => e.messages);
+    return errorResponse(req, res, messages.join(', '), 400, err);
+  }
+});
+app.use((req, res, next) => {
+  res.status(404).send({
+    code: 404,
+    errorMessage: 'Page not found',
+    error: {},
+    data: null,
+    success: false,
+  });
+});
 module.exports = app;
